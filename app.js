6004cdcb5801495cee47bd8f0e968b06bf0f7ef2
Kate Moser <komoser@gmail.com>
2022-02-16 14:14:01 -0600

    Finished /mean /mode /median

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
 /** Simple demo Express app. */
 
 const express = require("express");
+const { STATUS_CODES } = require("http");
 const app = express();
 
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
 
+//string array to num array function
+const { convertStrNums } = require("./utils")
+
+//import stats functions
+const { findMean } = require("./stats")
+const { findMedian } = require("./stats")
+const { findMode } = require("./stats")
+
+
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
+
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean/", function (req, res) {
+
+  //call function to turn string into array of nums
+  const nums = convertStrNums(req.query.nums);
+
+  //call mean function
+  const mean = findMean(nums);
+
+  // return res.send(`This is the mean of ${nums}: ${mean}`);
+  return res.json({
+    response: {
+      operation: "mean",
+      value: mean,
+    }
+  });
+});
 
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
+
+  const nums = convertStrNums(req.query.nums);
+
+  //call median function
+  const median = findMedian(nums);
+
+  // return res.send(`This is the median of ${nums}: ${median}`);
+  return res.json({
+    response: {
+      operation: "median",
+      value: median,
+    }
+  });
+});
 
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res) {
+
+  const nums = convertStrNums(req.query.nums);
+
+  //call mode function
+  const mode = findMode(nums);
+
+  // return res.send(`This is the mode of ${nums}: ${mode}`);
+  return res.json({
+    response: {
+      operation: "mode",
+      value: mode,
+    }
+  });
+});
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
@@ -31,6 +88,9 @@ app.use(function (err, req, res, next) {
   return res.status(status).json({ error: { message, status } });
 });
 
-
+function stringToNumArray(numString) {
+  const nums = numString.split(",").map(n => parseInt(n));
+  return nums;
+}
 
 module.exports = app;
--- utils.js
+++ utils.js
@@ -3,9 +3,23 @@ const { BadRequestError } = require("./expressError");
 
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
-function convertStrNums(strNums) {
+function convertStrNums(numString) {
+  if(!numString){
+    throw new BadRequestError("Nums are required");
+  }
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  const nums = numString.split(",").map(n => intifyOrError(n));
+
+  return nums;
+}
+
+function intifyOrError(str){
+  n = parseInt(str);
+  if (!n){
+    throw new BadRequestError(`${str} is not a number`);
+  }
+  return n;
 }
 
 
